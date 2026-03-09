9c5918d1bd3be3dd838a660c691fe0f48218c3d0
Tedd Onyesero <teddonyesero7@gmail.com>
2026-03-09 11:56:13 +0300

    feat: update environment variables for MongoDB Atlas and JWT, and enable Vercel Blob support.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  experimental: {
+    serverComponentsExternalPackages: ["@vercel/blob"],
+  },
 };
 
 module.exports = nextConfig;
